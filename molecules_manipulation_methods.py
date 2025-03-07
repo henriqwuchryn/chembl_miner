7ad4199e30f9f1c2628c84ed37655c84ee98238e
henrique wuchryn <henriq.gwm@gmail.com>
2025-03-07 17:06:30 -0300

    fixed mmm, import os added. machine learning started, still not functional

--- molecules_manipulation_methods.py
+++ molecules_manipulation_methods.py
@@ -1,6 +1,7 @@
 from chembl_webresource_client.new_client import new_client
 import pandas as pd
 import numpy as np
+import os
 from rdkit import Chem
 from rdkit.Chem import Descriptors, Lipinski
 from sklearn.feature_selection import VarianceThreshold
