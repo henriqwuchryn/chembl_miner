fce544a3f0c6a13353f630e0bfdfba746838ad89
henrique wuchryn <henriq.gwm@gmail.com>
2025-09-15 13:32:16 -0300

    updated .gitignore

--- example.py
+++ example.py
@@ -1,6 +1,12 @@
 import package_functions as pf
+import pandas as pd
 
-data = pf.get_activity_data(target_chembl_id="CHEMBL366",activity_type="MIC")
-print(pf.review_assays(data,assay_keywords=['ATCC'],n_to_show=50))
-
+yeasts_df = pd.DataFrame()
+data = pf.get_activity_data(target_chembl_id=["CHEMBL4513186"],activity_type="MIC")
+yeasts_df = pd.concat([yeasts_df, data], ignore_index=True)
+print('1')
+data = pf.get_activity_data(target_chembl_id=["CHEMBL612647"],activity_type="MIC")
+yeasts_df = pd.concat([yeasts_df, data], ignore_index=True)
+print('2')
+yeasts_df.to_csv('candidiasis_resistant.csv', index_label="index")
 
